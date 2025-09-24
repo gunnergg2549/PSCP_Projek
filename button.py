998fdf1479b8ed0faf2588bb7f7d7e03a0691874
gunnergg2549 <gunkritsada.pr@gmail.com>
2025-09-24 14:18:10 +0700

    Add files via upload

---
+++ button.py
@@ -0,0 +1,28 @@
+import pygame
+import sys
+
+pygame.init()
+main_font = pygame.font.SysFont("cambria", 50)
+
+class Button():
+	def __init__(self, image, x_pos, y_pos, text_input):
+		self.image = image
+		self.x_pos = x_pos
+		self.y_pos = y_pos
+		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+		self.text_input = text_input
+		self.text = main_font.render(self.text_input, True, "white")
+		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+	def update(self, surface):
+		surface.blit(self.image, self.rect)
+		surface.blit(self.text, self.text_rect)
+
+	def checkForInput(self, position):
+		return self.rect.collidepoint(position)
+
+	def changeColor(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			self.text = main_font.render(self.text_input, True, "green")
+		else:
+			self.text = main_font.render(self.text_input, True, "white")
--- mainmenu.py
+++ mainmenu.py
@@ -1,30 +1,41 @@
 import pygame
+import sys
+
+from button import Button
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 FPS = 60
 running = True
-pygame.display.set_caption("Mainmenu")
-BG = (50,50,50)
+BG = pygame.image.load("assets/faith-spark-background1.jpg")
 icon = pygame.image.load("assets/icon1.png")
+button_surface = pygame.image.load("assets/button.png")
+button_surface = pygame.transform.scale(button_surface, (700, 450))
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    #screen.fill(BG2)
-    screen.fill(BG)
-    pygame.display.set_icon(icon)
-    # RENDER YOUR GAME HERE
-    pygame.display.update()
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    clock.tick(FPS)  # limits FPS to 60
-
-pygame.quit()
+def main_menu():
+    pygame.display.set_caption("Mainmenu")
+    while running:
+        clock.tick(FPS)
+        pygame.display.set_icon(icon)
+        Exit_Button = Button(image=pygame.image.load("assets/button.png"), x_pos=(640), y_pos=(200),text_input="Exit")
+        Menu_mouse = pygame.mouse.get_pos()
+        screen.blit(BG,(0,0))
+        screen.fill("white")
+        
+        for button in [Exit_Button]:
+            button.changeColor(Menu_mouse)
+            button.update(screen)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Exit_Button.checkForInput(Menu_mouse):
+                    pygame.quit()
+                    sys.exit()
+        
+        button.update(screen)
+        pygame.display.update()
+main_menu()
