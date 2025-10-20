2fe954813288ce566cf620c6b51c9b1f779cc0c6
Bogie49 <bogiebismarck@gmail.com>
2025-10-20 12:44:55 +0700

    Add files via upload

---
+++ pop.gd
@@ -0,0 +1,24 @@
+extends Area2D
+export onready var sprite = $AnimatedSprite
+onready var gui = get_parent().get_node("GUI")
+var used:bool = false
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+func _ready():
+	sprite.play("blink")
+
+# Called when the node enters the scene tree for the first time.
+func _on_pop_body_entered(body):
+	if body.name == "Player" and !used and PlayerData.heart > 1:
+		give_hurt(body)
+		used = true
+
+func give_hurt(player):
+	player.damage()
+	for gui in get_tree().get_nodes_in_group("GUI"):
+		gui.update_hearts()
+	queue_free()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
